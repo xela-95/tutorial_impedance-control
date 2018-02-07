f19e66eced2874ee858148ba957ecbbcd31f775c
Silvio <silvio.traversaro@iit.it>
2018-02-07 17:54:50 +0100

    Cleanup spaces

--- Module.h
+++ Module.h
@@ -15,35 +15,35 @@
 
 class Module : public yarp::os::RFModule
 {
-    // Class to compute model quantities 
+    // Class to compute model quantities
     iDynTree::KinDynComputations kinDynModel;
-    
-    // Attributes to communicate with the robot YARP-based interface 
+
+    // Attributes to communicate with the robot YARP-based interface
     yarp::dev::PolyDriver robotDevice;
-    
-    // YARP Interfaces exposed by the remotecontrolboardremapper 
+
+    // YARP Interfaces exposed by the remotecontrolboardremapper
     yarp::dev::IControlLimits2   *ilim{nullptr};
     yarp::dev::IEncoders         *ienc{nullptr};
     yarp::dev::IControlMode2     *imod{nullptr};
     yarp::dev::ITorqueControl    *itrq{nullptr};
 
-    // Quantities used by the control 
+    // Quantities used by the control
     yarp::sig::Vector positionsInRad;
     yarp::sig::Vector velocitiesInRadS;
     yarp::sig::Vector positionsInDeg;
     yarp::sig::Vector velocitiesInDegS;
     yarp::sig::Vector gravityCompensation;
     yarp::sig::Vector referencePositionsInRad;
-        
+
     //write
     yarp::sig::Vector errorInRad;
-    yarp::sig::Vector kp; // Nm/rad 
+    yarp::sig::Vector kp; // Nm/rad
     yarp::sig::Vector kd; // Nm/rad
     yarp::sig::Vector torquesInNm;
     yarp::sig::Vector zeroDofs;
     yarp::sig::Vector baseZeroDofs;
     yarp::sig::Vector grav;
-    
+
 public:
     virtual double getPeriod ();
     virtual bool updateModule ();
